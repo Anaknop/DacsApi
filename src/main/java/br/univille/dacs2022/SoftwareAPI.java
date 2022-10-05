384197ac69eebd7065882a05f3c60011f926cfbb
Anaknop <48037371+Anaknop@users.noreply.github.com>
2022-10-05 15:58:19 -0300

    Trabalho API

---
+++ src/main/java/br/univille/dacs2022/SoftwareAPI.java
@@ -0,0 +1,13 @@
+package br.univille.dacs2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareAPI.class, args);
+	}
+
+}
